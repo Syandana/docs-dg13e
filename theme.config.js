af2f96078e9a441e3a5a533b66d994f7ee8aab92
Syandana <rizqy.syandana.p@gmail.com>
2021-04-10 10:38:33 +0700

    update theme

--- theme.config.js
+++ theme.config.js
@@ -14,7 +14,7 @@ export default {
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="Content-Language" content="en" />
+      <meta httpEquiv="Content-Language" content="id" />
       <meta name="description" content="emang beda" />
       <meta name="og:description" content="emang beda" />
       {/* <meta name="twitter:card" content="summary_large_image" /> */}
