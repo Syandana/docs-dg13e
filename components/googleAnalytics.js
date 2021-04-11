bec9cae88c553dbc7bca6132d2a07d7948d94ca1
Syandana <rizqy.syandana.p@gmail.com>
2021-04-11 15:34:11 +0700

    updated google analytics

--- components/googleAnalytics.js
+++ components/googleAnalytics.js
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga"
  
 export const initGA = () => {
-  ReactGA.initialize("G-T7H8BFQBPE")
+  ReactGA.initialize("UA-165036646-4")
 }
  
 export const logPageView = () => {
