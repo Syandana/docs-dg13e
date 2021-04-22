ab563de1169f59e809cb924f507d44b4941d44ba
Syandana <rizqy.syandana.p@gmail.com>
2021-04-22 13:43:19 +0700

    added google analytics & cvc document

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
 import 'nextra-theme-docs/style.css'
 import Layout from '../components/Layout'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
-export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps}>
-    <Layout></Layout>
-  </Component>
+// export default function Nextra({ Component, pageProps }) {
+//   return <Component {...pageProps}>
+//     <Layout></Layout>
+//   </Component>
+// }
+
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <Component {...pageProps} />
 }
+
+export default App
---
+++ pages/dokumen/creative-visual-concept.md
@@ -0,0 +1,24 @@
+# Creative Visual Concept
+
+>  Link Video Mas Ogie: https://youtu.be/VXnC7YNNIJs?t=2739
+
+- Client: Summarecon Mall Kelapa Gading
+- Theme: Hari Pendidikan Nasional
+- Target Audience: 15-30 Tahun
+- Artist Invited: JRock
+- Form of event: JRock musik sampe mall tutup
+- Draft Awal sebelum presentasi ke klien
+
+## Job Brief
+
+- Buat keywords acara
+- Buat judul acara
+- Buat tagline (harus ada 'atmosfir' Mall Kelapa Gading)
+- Buat logo acara (3 alternatif, gunakan keywords untuk acuan)
+- Buat desain poster promosi acara (2 versi)
+- Buat desain undangan acara (1 versi, gunakan keywords dan logo)
+- Disusun dalam bentuk proposal. Mirip kayak GSM waktu itu.
+
+
+
+>  logo mall + logo jrocks + judul acara = logo acara
