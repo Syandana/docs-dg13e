fac6d3c74ed8e8d777732fe0836013731bc7682b
Syandana <rizqy.syandana.p@gmail.com>
2021-05-03 20:28:41 +0700

    initial home page

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import Layout from "./Layout"
+
+const Index = (props) => (
+  <Layout>
+    <div>
+      <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+  </Layout>
+)
+
+export default Index
